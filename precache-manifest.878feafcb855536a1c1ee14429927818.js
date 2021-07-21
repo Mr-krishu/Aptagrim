c4083fb759d5450e8ff4e38c15909712a776e6aa
krishna kant <30586737+Mr-krishu@users.noreply.github.com>
2021-07-21 17:32:28 +0530

    Updates

---
+++ precache-manifest.878feafcb855536a1c1ee14429927818.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "223a350ae1d94a779cfdaba26fd389ae",
+    "url": "/Aptagrim/index.html"
+  },
+  {
+    "revision": "4ce25f68f2ee9818dad2",
+    "url": "/Aptagrim/static/css/main.00244def.chunk.css"
+  },
+  {
+    "revision": "e5e61f5e23338be75822",
+    "url": "/Aptagrim/static/js/2.f69e8b51.chunk.js"
+  },
+  {
+    "revision": "10be3df916f37e591bdb23bad1fa4ac4",
+    "url": "/Aptagrim/static/js/2.f69e8b51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ce25f68f2ee9818dad2",
+    "url": "/Aptagrim/static/js/main.8877dfe1.chunk.js"
+  },
+  {
+    "revision": "bb86edf7065f49439f11",
+    "url": "/Aptagrim/static/js/runtime-main.1b511bfc.js"
+  },
+  {
+    "revision": "836203446f9be9c082ad916323afb511",
+    "url": "/Aptagrim/static/media/avatar.83620344.jpg"
+  }
+]);
